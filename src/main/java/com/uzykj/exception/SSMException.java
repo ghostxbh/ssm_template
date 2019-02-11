3ff24bf558c24f6b2bbbf503d14c53b4c0221ee8
ghostxbh <ghostxbh@hotmail.com>
2019-02-11 21:39:46 +0800

    自定义异常处理类

---
+++ src/main/java/com/uzykj/exception/SSMException.java
@@ -0,0 +1,11 @@
+package com.uzykj.exception;
+
+/**
+ * @author ghostxbh
+ * @date 11/02/2019 20:58
+ */
+public class SSMException extends RuntimeException {
+    public SSMException(String message) {
+        super(message);
+    }
+}
